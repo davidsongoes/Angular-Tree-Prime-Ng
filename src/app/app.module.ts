4be20718ebbf2e75b20a96929a9693b306b60657
Davidson Góes <davidsongoes@gmail.com>
2022-04-18 21:11:49 -0300

    feat(setup): :wrench: Implementado recursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { ContextMenuModule } from 'primeng/contextmenu';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHandler,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { CardModule } from 'primeng/card';
+import { TreeModule } from 'primeng/tree';
+import { NodeService } from './node.service';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenuModule } from 'primeng/menu';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    CardModule,
+    TreeModule,
+    HttpClientModule,
+    FieldsetModule,
+    MenuModule,
+    ContextMenuModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NodeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
